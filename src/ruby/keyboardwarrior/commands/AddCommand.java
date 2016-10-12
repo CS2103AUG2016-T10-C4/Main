fd097869b450c560221b2afdc7757a2b4a267683
yongchenglow <lowyongcheng@hotmail.com>
2016-10-12 14:22:55 +0800

    Update the Add Command function
    Changed the message usage

--- src/ruby/keyboardwarrior/commands/AddCommand.java
+++ src/ruby/keyboardwarrior/commands/AddCommand.java
@@ -15,14 +15,19 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds a person to the address book. "
-            + "Contact details can be marked private by prepending 'p' to the prefix.\n\t"
-            + "Parameters: NAME [p]p/PHONE [p]e/EMAIL [p]a/ADDRESS  [t/TAG]...\n\t"
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds a task into the Keyboard Warrior.\n"
+            + "Parameters for Floating Task: TASK \n\t"
             + "Example: " + COMMAND_WORD
-            + " John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25 t/friends t/owesMoney";
+            + "add do the dishes\n"
+		    + "Parameters for Deadlines: TASK by [DATE] [TIME] \n\t"
+		    + "Example: " + COMMAND_WORD
+		    + "add do CS2103 Tutorial by Wednesday\n"
+            + "Parameters for Specific Task: DATE TIME [to TIME] TASK [@VENUE] \n\t"
+            + "Example: " + COMMAND_WORD
+            + "add 010616 1800 to 2000 Go to the Mall @Clementi Mall\n";
 
-    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_SUCCESS = "New task added: %1$s";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the Keyboard Warrior.\n";
 
     private final Task toAdd;
 
