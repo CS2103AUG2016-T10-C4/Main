a90e284d04048419ec2c7fc09028f4de443aa10b
marcusyatim <marcusyatim@gmail.com>
2016-10-27 19:32:45 +0800

    Update AddCommand function with Tag

--- src/ruby/keyboardwarrior/commands/AddCommand.java
+++ src/ruby/keyboardwarrior/commands/AddCommand.java
@@ -1,10 +1,14 @@
 package ruby.keyboardwarrior.commands;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import ruby.keyboardwarrior.data.TasksList;
 import ruby.keyboardwarrior.data.exception.IllegalValueException;
 import ruby.keyboardwarrior.data.task.*;
+import ruby.keyboardwarrior.data.tag.Tag;
+import ruby.keyboardwarrior.data.tag.UniqueTagList;
 
 /**
  * Adds a task to Keyboard Warrior.
@@ -14,18 +18,19 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
     public static final String DEADLINE_WORD = " by ";
     public static final String EVENT_WORD = " from ";
+    public static final String TAG_WORD = " is ";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Adds a task to the Keyboard Warrior. "
             + "Only supports task details which can be enter after; the command word seperated by a space. \n\t"
             + "To add a Todo: \n\t\t"
-            + "Format: add TASK \n\t\t"
-            + "Example: " + COMMAND_WORD + " do something\n\t"
+            + "Format: add TASK is [TAG] \n\t\t"
+            + "Example: " + COMMAND_WORD + " do something" + TAG_WORD + "leisure\n\t"
             + "To add a Deadline: \n\t\t"
-            + "Format: add TASK by DATE [TIME] \n\t\t"
+            + "Format: add TASK by DATE [TIME] is [TAG] \n\t\t"
             + "Example: " + COMMAND_WORD + " do something" + DEADLINE_WORD + "120416 1800\n\t"
             + "To add an Event: \n\t\t"
-            + "Format: add EVENT from STARTTIME [ENDTIME] \n\t\t"
-            + "Example: " + COMMAND_WORD + " some event" + EVENT_WORD + "221016 0900 221016 1700\n\t\t";
+            + "Format: add EVENT from STARTTIME [ENDTIME] is [TAG]\n\t\t"
+            + "Example: " + COMMAND_WORD + " some event" + EVENT_WORD + "221016 0900 221016 1700" + TAG_WORD + "school\n\t\t";
 
     public static final String MESSAGE_SUCCESS = "New task added: %1$s";
     public static final String MESSAGE_DUPLICATE_TASK = "This task already exists in the Keyboard Warrior";
@@ -38,7 +43,7 @@ public class AddCommand extends Command {
      * @throws IllegalValueException if any of the raw values are invalid
      */
 
-    public AddCommand(String details) throws IllegalValueException {
+    public AddCommand(String details, Set<String> tags) throws IllegalValueException {
     	/*
     	StringBuilder itemDetails = new StringBuilder();
      	StringBuilder startTime = new StringBuilder();
@@ -80,7 +85,11 @@ public class AddCommand extends Command {
         }
         scanType.close();
         */
-
+    	final Set<Tag> tagSet = new HashSet<>();
+        for (String tagName : tags) {
+            tagSet.add(new Tag(tagName));
+        }
+         
 		try{
 	    	String lowerCaseDetails = details.toLowerCase();
 			int byExist = lowerCaseDetails.lastIndexOf(DEADLINE_WORD);
@@ -88,19 +97,19 @@ public class AddCommand extends Command {
 				
 			if(byExist != -1 && Integer.parseInt(lowerCaseDetails.substring(byExist+4,byExist+10)) > 0){
 				if(details.length() > byExist+10)
-					this.toAdd = new Task(new TaskDetails(details.substring(0,byExist)), new DateTime(details.substring(byExist+4)));
+					this.toAdd = new Task(new TaskDetails(details.substring(0,byExist)), new DateTime(details.substring(byExist+4)), new UniqueTagList(tagSet));
 				else
-					this.toAdd = new Task(new TaskDetails(details.substring(0,byExist)), new Date(details.substring(byExist+4)));
+					this.toAdd = new Task(new TaskDetails(details.substring(0,byExist)), new Date(details.substring(byExist+4)), new UniqueTagList(tagSet));
 			}
 			else if(fromExist != -1){
-				this.toAdd = new Task(new TaskDetails(details.substring(0,fromExist)), new DateTime(details.substring(fromExist+6,fromExist+17)), new DateTime(details.substring(fromExist+18, fromExist+29)));
+				this.toAdd = new Task(new TaskDetails(details.substring(0,fromExist)), new DateTime(details.substring(fromExist+6,fromExist+17)), new DateTime(details.substring(fromExist+18, fromExist+29)), new UniqueTagList(tagSet));
 			}
 			else{
-				this.toAdd = new Task(new TaskDetails(details));
+				this.toAdd = new Task(new TaskDetails(details), new UniqueTagList(tagSet));
 			}
 		}
 		catch (NumberFormatException | StringIndexOutOfBoundsException siobe){
-			this.toAdd = new Task(new TaskDetails(details));
+			this.toAdd = new Task(new TaskDetails(details), new UniqueTagList(tagSet));
 		}
 		
     }
