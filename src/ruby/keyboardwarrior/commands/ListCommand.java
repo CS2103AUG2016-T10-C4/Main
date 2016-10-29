90d4a0c402caecf9dfc2ee123584e02abf63a8bc
yongchenglow <lowyongcheng@hotmail.com>
2016-10-29 17:24:35 +0800

    Update List Command
    Can now list different commands

--- src/ruby/keyboardwarrior/commands/ListCommand.java
+++ src/ruby/keyboardwarrior/commands/ListCommand.java
@@ -20,48 +20,51 @@ public class ListCommand extends Command {
     public static final String EVENT_TYPE = "event";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
-            + "Displays all items in the task manager as a list with index numbers.\n\t"
-            + "Example: " + COMMAND_WORD + "\n"
-		    + "Displays Todos in the task manager as a list with index numbers.\n\t"
-		    + "Example: " + COMMAND_WORD + "\n"
-		    + "Displays Deadlines in the task manager as a list with index numbers.\n\t"
-		    + "Example: " + COMMAND_WORD + "\n"
-		    + "Displays Events in the task manager as a list with index numbers.\n\t"
-		    + "Example: " + COMMAND_WORD + "\n";
+            + "Displays items in the task manager as a list with index numbers.\n\t"
+            + "To list all items:\n\t\t"
+            + COMMAND_WORD + "\n\t"
+            + "To list todos:\n\t\t"
+		    + COMMAND_WORD + " " + TODO_TYPE + "\n\t"
+		    + "To list deadlines:\n\t\t"
+		    + COMMAND_WORD + " " + DEADLINE_TYPE + "\n\t"
+		    + "To list events:\n\t\t"
+		    + COMMAND_WORD + " " + EVENT_TYPE;
     
     public static final String MESSAGE_LIST_TASK_SUCCESS = "List Item: ";
     
+    private Integer taskType;
+    
     public ListCommand(String arg){
-    	if(arg != null){
+    	if(arg.length() != 0){
     		if(TODO_TYPE.equalsIgnoreCase(arg.trim()))
-	    		execute(0);
+	    		this.taskType = 0;
 	    	else if(DEADLINE_TYPE.equalsIgnoreCase(arg.trim()))
-	    		execute(1);
+	    		this.taskType = 1;
 	    	else if(EVENT_TYPE.equalsIgnoreCase(arg.trim()))
-	    		execute(2);
+	    		this.taskType = 2;
 	    	else
-	    		execute(3);
+	    		this.taskType = 3;
 	    } else {
-	    	execute();
+	    	this.taskType = 4;
 	    }
 	}   
     
     @Override
     public CommandResult execute() {
         ArrayList<Task> allTasks = tasksList.getAllTasks();
-        return new CommandResult(getMessageForTasksListShownSummary(allTasks), allTasks);
-    }
-    
-    public CommandResult execute(Integer type) {
-    	ArrayList<Task> certainTask = new ArrayList<Task>();
-    	if(type != 3){
-	    	for(Task task : tasksList.getAllTasks()) {
-	                if(task.getTaskType() == type)
-	                	certainTask.add(task);
-        }
-	    	return new CommandResult(getMessageForTasksListShownSummary(certainTask), certainTask);
+        ArrayList<Task> certainTasks = new ArrayList<Task>();
+    	if(this.taskType != 4){
+	        if(this.taskType != 3){
+		    	for(Task task : tasksList.getAllTasks()) {
+		                if(task.getTaskType() == this.taskType)
+		                	certainTasks.add(task);
+	        }
+		    	return new CommandResult(getMessageForTasksListShownSummary(certainTasks), certainTasks);
+	    	} else {
+	    		return new CommandResult(MESSAGE_USAGE);
+	    	}
     	} else {
-    		return new CommandResult(getMessageForTasksListShownSummary(certainTask), certainTask);
+    		return new CommandResult(getMessageForTasksListShownSummary(allTasks), allTasks);
     	}
     }
     
